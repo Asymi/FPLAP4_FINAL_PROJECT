a0d37dbbba4de2dcc11666c4eb6f2842dafa2e85
Anna Tran <annatran@annas-air.mynet>
2020-09-29 12:55:25 +0100

    add new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
-// import { Activities, Categories, Dashboard, Login, Signup } from './containers'
-import Login from './containers/Login'
-import Signup from './containers/Signup'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Activities, Categories, Dashboard, Login, Signup, Landing, About, Covid19 } from './containers'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import NavBar from './components/NavBar'
+import './App.css'
 
 class App extends Component{
   render() {
     return (
       <div className="App">
+        <div className="logo-icon">
+            <Link to='/'><FontAwesomeIcon icon={faMapMarkerAlt}/>Trip Planner</Link>
+        </div>
         <NavBar></NavBar>
         <Switch>
-          {/* <Route exact path='/' component={Landing}/>
+          <Route exact path='/' component={Landing}/>
           <Route path='/about' component={About}/>
           <Route path='/activities' component={Activities}/>
           <Route path='/categories' component={Categories}/>
-          <Route path='/dashboard' component={Dashboard}/> */}
+          <Route path='/dashboard' component={Dashboard}/>
           <Route path='/login' component={Login}/>
           <Route path='/signup' component={Signup}/>
+          <Route path='/covid' component={Covid19}/>
         </Switch>
+        <footer>
+          &copy; 2020 APP NAME All rights reserved.
+        </footer>
       </div>
     )
   }
