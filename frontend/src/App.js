ddc15813bd50a81985f2f744368bafdda13057a8
Anna Tran <annatran@annas-air.mynet>
2020-09-30 17:10:53 +0100

    remove categories route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom'
-import { Countries, Categories, Dashboard, Login, Signup, Landing, About, Covid19, ForgotPassword } from './containers'
+import { Countries, Dashboard, Login, Signup, Landing, About, Covid19, ForgotPassword, NotFound404 } from './containers'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { connect } from 'react-redux'
@@ -26,13 +26,13 @@ class App extends Component{
           <Route exact path='/' component={Landing}/>
           <Route path='/about' component={About}/>
           <Route path='/countries/:slug' component={Countries}/> 
-          <Route path='/categories' component={Categories}/>
           <Route path='/dashboard' component={Dashboard}/>
           <Route path='/login' component={Login}/>
           <Route path='/signup' component={Signup}/>
           <Route path='/forgot' component={ForgotPassword}/>
           <Route path='/covid' component={Covid19}/>
           <Route path='/forgotpassword' component={ForgotPassword}/>
+          <Route component={NotFound404}/>
         </Switch>
         <footer>
           &copy; 2020 Find-Do All rights reserved.
