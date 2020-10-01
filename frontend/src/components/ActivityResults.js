ad0ff74feec2d140d06a4d72fe0176fb32f645d8
Anna Tran <annatran@annas-air.mynet>
2020-10-01 18:11:05 +0100

    map results

--- frontend/src/components/ActivityResults.js
+++ frontend/src/components/ActivityResults.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
+import './styles/ActivityResultsStyle.css'
 
 class ActivityResults extends Component{
 
@@ -11,24 +12,20 @@ class ActivityResults extends Component{
         const allData = this.props.results.map((item, idx) =>
             <div key={idx} className="activity-container">
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180?text=Image" />
+                    <Card.Img variant="top" src="./assets/baguette.jpg" />
                     <Card.Body>
-                        <Card.Title>Activity name - {item.name}</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
+                        <Card.Title>{item.name}</Card.Title>
                     </Card.Body>
                     <ListGroup className="list-group-flush">
-                        <ListGroup.Item>{item.price}</ListGroup.Item>
-                        <ListGroup.Item>{item.opening_hours}</ListGroup.Item>
-                        <ListGroup.Item>{item.provider}</ListGroup.Item>
-                        <ListGroup.Item>{item.address}</ListGroup.Item>
-                        <ListGroup.Item>{item.phone_number}</ListGroup.Item>
+                        <ListGroup.Item>Opening Hours: {item.opening_hours}</ListGroup.Item>
+                        <ListGroup.Item>Address: {item.address}</ListGroup.Item>
+                        <ListGroup.Item>Price: {item.price}</ListGroup.Item>
+                        <ListGroup.Item>Provider: {item.provider}</ListGroup.Item>
+                        <ListGroup.Item>Contact Number: {item.phone_number}</ListGroup.Item>
                     </ListGroup>
-                    <Card.Body>
+                    {/* <Card.Body>
                         <Card.Link href="#">Website?</Card.Link>
-                    </Card.Body>
+                    </Card.Body> */}
                 </Card>
                 
             </div>) 
@@ -36,6 +33,24 @@ class ActivityResults extends Component{
         return (
             <div className="all-habits">
                 {allData} 
+                {/* <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180?text=Image" />
+                    <Card.Body>
+                        <Card.Title>Activity name</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroup.Item>price</ListGroup.Item>
+                        <ListGroup.Item>opening hours</ListGroup.Item>
+                        <ListGroup.Item>provider</ListGroup.Item>
+                        <ListGroup.Item>address</ListGroup.Item>
+                        <ListGroup.Item>phone number</ListGroup.Item>
+                    </ListGroup>
+            
+                </Card> */}
             </div>
         )
     }
