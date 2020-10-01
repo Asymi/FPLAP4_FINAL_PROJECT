8b06db7b6b89048a43e4151d85db17fd296ddf47
Anna Tran <annatran@annas-air.mynet>
2020-10-01 18:12:10 +0100

    add classnames to elements

--- frontend/src/components/WarningText.js
+++ frontend/src/components/WarningText.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './styles/WarningTextStyle.css'
+// import { TiWarning } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { TiWarning } from "react-icons/ti"
 
 const WarningText = () => {
     return (
         <div className="warning-banner">
-            <Link to='/covid'>Travel notice. Learn more about COVID-19</Link>
+            <Link to='/covid' className="covid-text"><TiWarning/> Travel notice. Learn more about COVID-19</Link>
         </div>
     )
 }
