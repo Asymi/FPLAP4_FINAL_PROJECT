34ff3fcf90df7063501149303d5f57533131a629
Anna Tran <annatran@annas-air.mynet>
2020-10-02 08:46:52 +0100

    remove stylesheet

--- frontend/src/components/ActivityResults.js
+++ frontend/src/components/ActivityResults.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
-import './styles/ActivityResultsStyle.css'
 
 class ActivityResults extends Component{
 
@@ -23,9 +22,6 @@ class ActivityResults extends Component{
                         <ListGroup.Item>Provider: {item.provider}</ListGroup.Item>
                         <ListGroup.Item>Contact Number: {item.phone_number}</ListGroup.Item>
                     </ListGroup>
-                    {/* <Card.Body>
-                        <Card.Link href="#">Website?</Card.Link>
-                    </Card.Body> */}
                 </Card>
                 
             </div>) 
@@ -33,24 +29,6 @@ class ActivityResults extends Component{
         return (
             <div className="all-habits">
                 {allData} 
-                {/* <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180?text=Image" />
-                    <Card.Body>
-                        <Card.Title>Activity name</Card.Title>
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item>price</ListGroup.Item>
-                        <ListGroup.Item>opening hours</ListGroup.Item>
-                        <ListGroup.Item>provider</ListGroup.Item>
-                        <ListGroup.Item>address</ListGroup.Item>
-                        <ListGroup.Item>phone number</ListGroup.Item>
-                    </ListGroup>
-            
-                </Card> */}
             </div>
         )
     }
